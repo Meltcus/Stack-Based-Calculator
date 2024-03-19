69c0a8427b55e5ae21086d6da37bc6ee9fbc2233
Meltcus <139188612+Meltcus@users.noreply.github.com>
2024-03-19 12:36:41 -0400

    Main.java
    Implemented all types of Inequalities - Implemented Files Input and Output - Amortixed Push Operation to O(1) - Shrinked Array for Effective Storage Use

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,67 @@
-// Rough Draft
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
-        String A = "10/5>1";
+        String A = "5 + 3 * (2 - 4)";
 
         System.out.println(SolveExpression(A));
 
-        // TODO: Implement "==" and "!="
-        // TODO: Implement Files
-        // TODO: Make it respect the rules
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("Arithmetic.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("ArithmeticAnswer.txt"));
 
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Process each line read from the file
+                System.out.println("Expression is: " + line);
+                bw.write("Expression: " + line + "\n");
+
+                System.out.println("Answer of the Expression is: " + SolveExpression(line));
+                bw.write("Answer: " + SolveExpression(line) + "\n");
+
+                System.out.println();
+                bw.newLine();
+            }
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("File Could not be opened");
+        }
+
+    }
+
+    /** This helper method takes care of any cases of "==" or "!=" and converts them to a character '=' or '!' for simplicty in calculation
+     *
+     * @param arithmetic the String which may or may not be modified if necessary
+     * @return The proper arthritic expression to calculate
+     */
+    public static String SolveExpression (String arithmetic) {
+
+        arithmetic = arithmetic.replace("==","=");
+        arithmetic = arithmetic.replace("!=", "!");
+
+        return SolveExpressionHelper(arithmetic);
 
 
     }
 
 
-    public static int SolveExpression(String arithmetic) {
+    /** Solves the Arithmetic Expression utilized two stacks
+     *
+     * @param arithmetic the String that has been converted from its "==" to '=' and "!=" to '!'
+     * @return a String Answer ( a number or "True" or "False")
+     */
+    public static String SolveExpressionHelper(String arithmetic) {
+
         IntegerStack value_stack = new IntegerStack();      // int stack
         Stack operating_stack = new Stack();                // char stack
+
+        boolean hasInequality = false;
 
         for (int i = 0; i < arithmetic.length(); i++) {
             char token = arithmetic.charAt(i);
@@ -42,7 +86,9 @@ public class Main {
             } // end of if statement
             else if (token == '(') {
                 operating_stack.push('(');
+
             } else if (token == ')') {
+
                 while (operating_stack.peek() != '(') { // loop until we find the opening brackets
                     int num1, num2;
                     char operator;
@@ -58,7 +104,11 @@ public class Main {
                 // remove the "(" from the stack
                 operating_stack.pop();
             } // end of if-else statement
+
+
+
             else if (isOperator(token)) {
+
                 while (!operating_stack.isEmpty() && precedence(operating_stack.peek()) >= precedence(token)) {
                     int num1, num2;
                     char operator;
@@ -70,9 +120,14 @@ public class Main {
                     // Perform the operation and push the result back onto the value stack
                     value_stack.push(applyOperation(operator, num1, num2));
                 }
+
                 // Push the current operator onto the stack.
                 operating_stack.push(token);
+
+
             } // end of else if statement
+
+
         } // end of for-loop
 
         // Now clear the remaining elements of the stack (like inserting $ into the stack from Notes)
@@ -83,18 +138,39 @@ public class Main {
             int num1 = value_stack.pop(); // Pop the first operand 4
             int num2 = value_stack.pop(); // Pop the second operand 5
 
+            if (operator == '>' || operator == '<' || operator == '≥' || operator == '≤' || operator == '!' || operator == '=')
+                hasInequality = true;
+
             // Apply the operation and push the result back onto the stack
             int result = applyOperation(operator, num1, num2);
             value_stack.push(result);
         }
 
         // Return the final result
-        return value_stack.pop();
+
+        if(hasInequality) {
+            if (value_stack.pop() == 1)
+                return "True";
+            else
+                return "False";
+        }
+        else
+            return "" + value_stack.pop();
     }
 
 
+
+    /** Checks the value of the precedence
+     *
+     * @param operator is a char
+     * @return the value of the precedence
+     */
     public static int precedence(char operator) {
         switch (operator) {
+
+            case '=':
+            case '!':
+                return 1;
             case '>':
             case '<':
             case '≥':
@@ -113,6 +189,8 @@ public class Main {
         }
     }
 
+
+
     /** This method will let us know if the operator is a valid or invalid operator
      *
      * @param op the supposed operator character
@@ -120,11 +198,19 @@ public class Main {
      */
     public static boolean isOperator(char op) {
         return op == '+' || op == '-' || op == '*' || op == '/' || op == '^'
-                || op == '>' || op == '<' || op == '≥' || op == '≤';
+                || op == '>' || op == '<' || op == '≥' || op == '≤' || op == '=' || op == '!';
     }
 
 
 
+
+    /** Will do the operation
+     *
+     * @param operator is a char of the supposed operater
+     * @param b is int value
+     * @param a is another int value
+     * @return the final value of the this simple expression
+     */
     public static int applyOperation(char operator, int b, int a) {
         switch (operator) {
             case '+':
@@ -138,6 +224,7 @@ public class Main {
                 return b / a;
             case '^':
                 return (int) Math.pow(a, b);
+
             case '>':
                 return a > b ? 1 : 0;   // If 1 then a > b and 0
             case '<':
@@ -146,6 +233,12 @@ public class Main {
                 return a >= b ? 1 : 0;
             case '≤':
                 return a <= b ? 1 : 0;
+
+            case '=':
+                return a == b ? 1 : 0; // If 1 then a == b
+            case '!':
+                return a != b ? 1 : 0; // If 1 then a != b
+
             default:
                 throw new IllegalArgumentException("Invalid operator: " + operator);
         }
